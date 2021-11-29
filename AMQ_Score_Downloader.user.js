198ff02ca69ae45a909c7ea9e836d824e70d1891
Jib <lukas.buecker@rwth-aachen.de>
2021-11-29 18:17:53 +0100

    <Deleted unnecessary File and changed Version to a better number>

--- AMQ_Score_Downloader.user.js
+++ AMQ_Score_Downloader.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         AMQ Score Download
-// @version      1.4.0
+// @version      1.0.5
 // @description  Displays Score Final in AMQ of all Players and downloads a json of them
 // @author       Jib
 // @match        https://animemusicquiz.com/*
